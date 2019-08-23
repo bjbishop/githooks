950664b35356484111c0f8d3c1af73f9d3448fb3
Viktor Adam <rycus86@gmail.com>
2019-08-23 22:35:23 +1000

    Windows tests

--- tests/exec-steps.sh
+++ tests/exec-steps.sh
@@ -32,7 +32,7 @@ for STEP in /var/lib/tests/step-*.sh; do
 
     TEST_RUNS=$((TEST_RUNS + 1))
 
-    TEST_OUTPUT=$(sh "$STEP" 2>&1)
+    TEST_OUTPUT=$(sh -x "$STEP" 2>&1 | tee /dev/stderr)
     TEST_RESULT=$?
     # shellcheck disable=SC2181
     if [ $TEST_RESULT -eq 249 ]; then
